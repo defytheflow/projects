c48b796bb3f13d9828fae5ddde63f3e04709dad1
Artyom Danilov <defytheflow@gmail.com>
2020-06-21 22:17:09 +0300

    Increase speed of wordcollect project

--- wordcollect/wordcollect.py
+++ wordcollect/wordcollect.py
@@ -3,9 +3,11 @@
 '''
     File:     wordcollect.py
     Created:  23.06.2019
+    Updated:  21.06.2020
     Author:   Artyom Danilov
 '''
 
+import bisect
 import random
 import readline
 import sys
@@ -14,49 +16,91 @@ import string
 import colorama
 
 
-COMMANDS = {
-    'found':    'f',
-    'mistakes': 'm',
-    'quit':     'q',
-    'words':    'w',
-}
+class Commands:
+
+    FOUND    = 'f'
+    MISTAKES = 'm'
+    QUIT     = 'q'
+
+    @classmethod
+    def is_command(cls, string):
+        return string in cls.__dict__.values()
+
+    @classmethod
+    def execute(cls, command, game):
+        if command == cls.FOUND:
+            game.printer.info(f'Found words: {game.user.found_words}')
+        elif command == cls.MISTAKES:
+            game.printer.info(f'Mistakes: {game.user.mistakes}')
+        elif command == cls.QUIT:
+            sys.exit(0)
+        else:
+            raise ValueError("Unknown command - '{command}'")
+
+
+class Settings:
+
+    MISTAKES_LIMIT  = 10
+    WORDS_LIMIT     = 10
+    WORD_MIN_LENGTH = 4
+
+
+class Utils:
+
+    @staticmethod
+    def bsearch(seq, item):
+        i = bisect.bisect_left(seq, item)
+        return i if (i != len(seq) and seq[i] == item) else -1
+
+    @staticmethod
+    def keychoice(seq, key):
+        while True:
+            element = random.choice(seq)
+            if key(element):
+                return element
+
+    @staticmethod
+    def is_ascii_lowercase(word):
+        return all(char in string.ascii_lowercase for char in word)
+
+    @staticmethod
+    def freadlines(fname):
+        with open(fname) as file:
+            return file.readlines()
 
 
 class Printer:
 
+    colors = {
+        'error':   colorama.Fore.RED,
+        'info':    colorama.Fore.BLUE,
+        'primary': colorama.Fore.MAGENTA,
+        'success': colorama.Fore.GREEN,
+        'warning': colorama.Fore.YELLOW,
+    }
+
     def __init__(self):
         colorama.init()
 
-    def error(self, *args, prefix=False, command=None):
-        if command is not None:
-            command()
-        print(colorama.Fore.RED, 'error: ' if prefix else '', sep='', end='')
-        print(*args, colorama.Style.RESET_ALL)
+    def error(self, *args, **kwargs):
+        self._print('error', *args, **kwargs)
 
-    def info(self, *args, prefix=False, command=None):
-        if command is not None:
-            command()
-        print(colorama.Fore.BLUE, 'info: ' if prefix else '', sep='', end='')
-        print(*args, colorama.Style.RESET_ALL)
+    def info(self, *args, **kwargs):
+        self._print('info', *args, **kwargs)
 
-    def success(self, *args, prefix=False, command=None):
-        if command is not None:
-            command()
-        print(colorama.Fore.GREEN, 'success: ' if prefix else '', sep='', end='')
-        print(*args, colorama.Style.RESET_ALL)
+    def primary(self, *args, **kwargs):
+        self._print('primary', *args, **kwargs)
 
-    def warning(self, *args, prefix=False, command=None):
-        if command is not None:
-            command()
-        print(colorama.Fore.YELLOW, 'warning: ' if prefix else '', sep='', end='')
-        print(*args, colorama.Style.RESET_ALL)
+    def success(self, *args, **kwargs):
+        self._print('success', *args, **kwargs)
 
-    def primary(self, *args, command=None):
-        if command is not None:
-            command()
-        print(colorama.Fore.MAGENTA, sep='', end='')
-        print(*args, colorama.Style.RESET_ALL)
+    def warning(self, *args, **kwargs):
+        self._print('warning', *args, **kwargs)
 
+    def _print(self, category, *args, prefix=False, command=lambda: None, **kwargs):
+        command()
+        print(self.colors[category], f'{category.title()}: ' if prefix else '', sep='', end='')
+        print(*args, colorama.Style.RESET_ALL, **kwargs)
 
 
 class User:
@@ -75,69 +119,24 @@ class MistakesLimitExceededError(Exception):
 
 class Game:
 
-    MISTAKES_LIMIT = 10
-    WORDS_LIMIT = 10
-    WORD_MIN_LENGTH = 4
-
     def __init__(self):
         self.user = User()
         self.printer = Printer()
-
-        self.game_word_dict = self.cleared_data_for_game_word('words.txt')
-        self.game_word = self.generate_word()
-        self.check_word_dict = self.cleared_data_to_check_user_word('words.txt')
-
+        self.words = Utils.freadlines('words.txt')
+        self.game_word = Utils.keychoice(self.words, key=lambda word: len(word) >= 10).strip()
         self.print_rules()
 
-    def cleared_data_for_game_word(self, dict: str) -> list:
-        """dict for generating game word"""
-        with open(dict) as eng_words:
-            raw_data = eng_words.readlines()
-
-            def validate(word: str) -> bool:
-                if '-' in word or "'" in word or len(word.strip(' ').strip('\n')) <= 15:
-                    return False
-                return True
-
-            clear_data = [word.strip(' ').rstrip('\n').lower() for word in raw_data if validate(word)]
-        return clear_data
-
-    def cleared_data_to_check_user_word(self, dict: str) -> list:
-        """dict to check user found word"""
-        with open(dict) as eng_words:
-            raw_data = eng_words.readlines()
-
-            def validate(word: str) -> bool:
-                if '-' in word or "'" in word or len(word.strip(' ').strip('\n')) < 4 \
-                        or len(word.strip(' ').strip('\n')) > len(self.game_word):
-                    return False
-                return True
-
-            clear_data = [word.strip(' ').rstrip('\n').lower() for word in raw_data if validate(word)]
-        return clear_data
-
-    def generate_word(self) -> str:
-        """generates word from check dict"""
-        random_word = random.choice(self.game_word_dict)
-        return random_word
-
-    def execute_command(self, command):
-        if command == COMMANDS['found']:
-            self.printer.info(f'Found words: {self.user.found_words}')
-        elif command == COMMANDS['mistakes']:
-            self.printer.info(f'Mistakes: {self.user.mistakes}')
-        elif command == COMMANDS['quit']:
-            sys.exit(0)
-        elif command == COMMANDS['words']:
-            self.printer.info(f'Words left: {self.WORDS_LIMIT - len(self.user.found_words)}')
-        else:
-            raise ValueError("Unknown command - '{command}'")
-
-    def is_english(self, word):
-        for char in word:
-            if char not in string.ascii_lowercase:
-                return False
-        return True
+    def mainloop(self):
+        while True:
+            try:
+                self.prompt_user('Found word: ')
+            except MistakesLimitExceededError:
+                self.printer.error('Game over! You lost!')
+                break
+            else:
+                if len(self.user.found_words) == Settings.WORDS_LIMIT:
+                    self.printer.success('Game over! You won!')
+                    break
 
     def word_can_be_composed(self, word):
         game_word = list(self.game_word)
@@ -150,66 +149,50 @@ class Game:
 
     def prompt_user(self, msg):
         ''' Main method that does all the validation of user input. '''
-
         while True:
 
-            if self.user.mistakes == self.MISTAKES_LIMIT:
+            if self.user.mistakes == Settings.MISTAKES_LIMIT:
                 raise MistakesLimitExceededError()
 
-            self.printer.primary(f'\n{self.game_word.upper()}\n')
-            user_word = input(msg).strip().lower()
-
-            if user_word in COMMANDS.values():
-                self.execute_command(user_word)
-                continue
+            self.printer.primary(f'[ {self.game_word.upper()} ]: ', end='')
+            user_word = input().strip().lower()
 
             if not user_word:
-                self.printer.error('blank input.', prefix=True, command=self.user.inc_mistakes)
+                self.printer.error('blank input.\n', prefix=True, command=self.user.inc_mistakes)
                 continue
 
-            if not user_word.isalpha():
-                self.printer.error('invalid word.', prefix=True, command=self.user.inc_mistakes)
+            if Commands.is_command(user_word):
+                Commands.execute(user_word, self)
                 continue
 
-            if not self.is_english(user_word):
-                self.printer.error('invalid english word.', prefix=True, command=self.user.inc_mistakes)
+            if not Utils.is_ascii_lowercase(user_word):
+                self.printer.error('invalid english word.\n', prefix=True, command=self.user.inc_mistakes)
                 continue
 
-            if len(user_word) < self.WORD_MIN_LENGTH:
-                self.printer.error('short word.', prefix=True, command=self.user.inc_mistakes)
+            if len(user_word) < Settings.WORD_MIN_LENGTH:
+                self.printer.error('short word.\n', prefix=True, command=self.user.inc_mistakes)
                 continue
 
             if len(user_word) >= len(self.game_word):
-                self.printer.error('long word.', prefix=True, command=self.user.inc_mistakes)
+                self.printer.error('long word.\n', prefix=True, command=self.user.inc_mistakes)
                 continue
 
-            if user_word not in self.check_word_dict:
-                self.printer.error('word does not exist in the game dictionary.',
+            if Utils.bsearch(self.words, f'{user_word}\n') == -1:
+                self.printer.error('word does not exist in the game dictionary.\n',
                                    prefix=True,
                                    command=self.user.inc_mistakes)
                 continue
 
             if user_word in self.user.found_words:
-                self.printer.error('word already found.', prefix=True, command=self.user.inc_mistakes)
+                self.printer.error('word already found.\n', prefix=True, command=self.user.inc_mistakes)
                 continue
 
             if not self.word_can_be_composed(user_word):
-                self.printer.error('cannot compose word.', prefix=True, command=self.user.inc_mistakes)
+                self.printer.error('cannot compose word.\n', prefix=True, command=self.user.inc_mistakes)
                 continue
 
             self.printer.success('correct!', command=lambda: self.user.found_words.append(user_word))
 
-    def mainloop(self):
-        while True:
-            try:
-                self.prompt_user('Found word: ')
-            except MistakesLimitExceededError:
-                self.error('Game over! You lost!')
-                break
-            else:
-                if len(self.user.found_words) == self.WORDS_LIMIT:
-                    self.success('Game over! You won!')
-                    break
 
     @staticmethod
     def print_rules():
@@ -228,7 +211,8 @@ class Game:
             '  * f - show found words',
             '  * q - quit',
 
-        sep='\n', end='\n\n')
+            sep='\n', end='\n\n'
+        )
 
 
 if __name__ == '__main__':
